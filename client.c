14e4ceac0f6633da4e6508fda72fe8e5aeb7eea0
Satchin Campbell <satchin.campbell@gmail.com>
2019-02-14 16:55:37 +0000

    add network headers to new client file

---
+++ client.c
@@ -0,0 +1,4 @@
+#include <sys/socket.h>       /*  socket definitions        */
+#include <sys/types.h>        /*  socket types              */
+#include <arpa/inet.h>        /*  inet (3) funtions         */
+#include <unistd.h>           /*  misc. UNIX functions      */
