338d249736b17f731bdcf729e83753982f784f83
Satchin Campbell <satchin.campbell@gmail.com>
2019-02-15 04:22:46 +0000

    Add required headers

--- client.c
+++ client.c
@@ -2,3 +2,9 @@
 #include <sys/types.h>        /*  socket types              */
 #include <arpa/inet.h>        /*  inet (3) funtions         */
 #include <unistd.h>           /*  misc. UNIX functions      */
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
